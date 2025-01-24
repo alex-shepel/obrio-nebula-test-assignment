ba1e80b720681f3a315369ff69172f991937a74b
Oleksandr Shepel <oleksandr.sh@mate.academy>
2025-01-24 12:37:37 +0200

    add env config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ListController } from './list/list.controller';
+import { GoogleDriveService } from './google-drive/google-drive.service';
+import { GoogleAuthService } from './google-auth/google-auth.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController, ListController],
-  providers: [AppService],
+  providers: [AppService, GoogleDriveService, GoogleAuthService],
 })
 export class AppModule {}
