d2837263c10ef8e2e6ffa56d69efa0bbcc44151b
Oleksandr Shepel <oleksandr.sh@mate.academy>
2025-01-26 16:33:26 +0200

    limit google-drive service to work with working directory only

--- src/google-drive/google-drive.service.ts
+++ src/google-drive/google-drive.service.ts
@@ -4,27 +4,56 @@ import { GoogleAuthService } from '../google-auth/google-auth.service';
 
 @Injectable()
 export class GoogleDriveService {
+  private static readonly WORKING_DIRECTORY_NAME = 'obrio-nebula-test-assignment';
+
   private drive;
+  private workingDirectoryId: string;
 
   constructor(private readonly authService: GoogleAuthService) {
-    this.initializeDrive();
+    this.init();
   }
 
-  private async initializeDrive() {
+  private async init() {
+    await this.initDrive();
+    await this.initWorkingFolder();
+  }
+
+  private async initDrive() {
     const authClient = await this.authService.getAuthClient();
 
     this.drive = google.drive({
       version: 'v3',
       auth: authClient,
     });
+
+    await this.initWorkingFolder();
   }
 
-  // TODO: add types
+  private async initWorkingFolder(): Promise<void> {
+    const existingWorkingFolderId = await this.getWorkingFolderId();
+
+    if (existingWorkingFolderId) {
+      this.workingDirectoryId = existingWorkingFolderId;
+
+      return;
+    }
+
+    const { data: createdFolder } = await this.drive.files.create({
+      requestBody: {
+        name: GoogleDriveService.WORKING_DIRECTORY_NAME,
+        mimeType: 'application/vnd.google-apps.folder',
+      },
+      fields: 'id',
+    });
+
+    this.workingDirectoryId = createdFolder.id;
+  }
+
   async listFiles(): Promise<any> {
     try {
       const response = await this.drive.files.list({
-        fields: 'files(mimeType,id,name,webViewLink,parents)',
-        pageSize: 10,
+        q: `'${this.workingDirectoryId}' in parents and trashed=false`,
+        fields: 'files(mimeType,id,name,webViewLink)',
       });
 
       return response.data.files;
