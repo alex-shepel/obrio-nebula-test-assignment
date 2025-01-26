a74074bce53c6b0eae78289be8b40ba0e198f1ab
Oleksandr Shepel <oleksandr.sh@mate.academy>
2025-01-26 16:51:54 +0200

    remove redundant files and tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ListController } from './list/list.controller';
 import { GoogleDriveService } from './google-drive/google-drive.service';
 import { GoogleAuthService } from './google-auth/google-auth.service';
@@ -9,7 +7,7 @@ import { SaveController } from './save/save.controller';
 
 @Module({
   imports: [ConfigModule.forRoot()],
-  controllers: [AppController, ListController, SaveController],
-  providers: [AppService, GoogleDriveService, GoogleAuthService],
+  controllers: [ListController, SaveController],
+  providers: [GoogleDriveService, GoogleAuthService],
 })
 export class AppModule {}
